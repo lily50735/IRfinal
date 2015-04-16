c52b815871d39da092b6ae8581ba48b39b8a6fe2
Lily Yu <chy59@pitt.edu>
2015-04-15 20:21:19 -0400

    yo

--- web/js/newjavascript.js
+++ web/js/newjavascript.js
@@ -17,7 +17,7 @@ function show()
     xhr.onreadystatechange = callback;
 //    console.log("heere");
 //    input=document.getElementById("input");
-    xhr.open("GET", "https://graph.facebook.com/search?q=lily&type=user&access_token=CAAIfBHv6dfoBAFA1k6aoiK0jP5J37Gt8AmWcqMsNe0fcmcliTJSBMwSx4ZAAjnabOIs2YgYDAGfZCydLRJwgp1abvi3zM1JGZAaOU0hvdQnGgGPyzSvwMXXEAQmdNzuEjZCSlD9UZAywmtqc7k6SgNQJFOLS88zjpFoBIAIlW8bXVDyLI1BUE4RzbBuvX8DKE9cyZCUl2Uz6VmZBTAJdwOc", true);
+    xhr.open("GET", "https://graph.facebook.com/search?q=joe&type=user|post&access_token=CAACEdEose0cBAD7hKZBQPbgFZAx1OrNrwu4xPgrlflqHHoc2l7JZBpgTFZCzZBIHIj9ZCR8PIFWqZAr1pEVtxcFaEBrYOKdZC4EtgNZBfEZAwsiln3cVm1Y2mJ8fojYdZBaGzNg4VcDFGCVNbeM7PPLXZBhto7KSOyHx2hLWZAOKHBUhWRYeZCiyTAZBFYtRvS96h14ZC1Q92aauXcF68FatZAL8pDgIK", true);
 //    xhr.open("GET", "https://graph.facebook.com/me?access_token=CAACEdEose0cBAGNYmIKzi5rd2GFn5oNRREa8wIDXgStaPADXJZCiVoEw5hBSoAPPkLWQzNLhKZAY9qygpZCL44T2N90gGDmpR2Gq4z8sTzkOneQzxq4LXK7MxnezPbPj5RyJYxUxQGl3XZBVi3tGGYyFg6ShXMf5FWKTm4441fxxUzwfNfQpfoqksj8LYJrdbkTiZBy71ajtsZAmRFwhCw", true);
 
     xhr.send();
