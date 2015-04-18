7c46c51218db4ab52b25edde26ff1ff8bb051ad3
Lily Yu <chy59@pitt.edu>
2015-04-18 17:18:21 -0400

    .

--- build/web/js/newjavascript.js
+++ build/web/js/newjavascript.js
@@ -3,81 +3,176 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var xhr1, xhr2;
 
-var xhr;
-function show()
-{
-    if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
-        xhr=new XMLHttpRequest();
-    }
-    else{// code for IE6, IE5
-        xhr=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xhr.onreadystatechange = callback;
-//    console.log("heere");
-//    input=document.getElementById("input");
-    var body= document.getElementsByTagName('BODY')[0];
+function show() {
+    var body = document.getElementsByTagName('BODY')[0];
     var query = document.getElementById("query").value;
-    var d = document.createElement("div");
-    xhr.open("GET", "https://graph.facebook.com/search?q="+ query +"&type=place&fields=id,name,picture&access_token=CAACEdEose0cBALwazDtNRu65wT3KsrmoUHN3mFU9MJK7c263ZBotjuTNra8xTH82gXrN1QmfbfLEOM4837z7tZBZBT3vZCJB1WQsLQ6Uda2VPFSu81FBZC2Y8LnmQZBqbfiiPdH91MnFwC43oeq8RYXcpHFeJlm8np3Uc3Y1AODB8URZBio1PuIjQd1VDXdJEzZCRBkjUe0OePntvNm3iGbq", true);
-    
-    // xhr.open("GET", "https://graph.facebook.com/search?q="+ query +"joe&type=user&type=page&type=place&type=event&type=group&type=placetopic&access_token=CAACEdEose0cBAPzXP7259eqrPOR0Y5k2yluEjLjOI05hM1pzucqy4WmrwizhnUX5ZAJTZBkVvVSyFQkCL0NRKh1Pn7IsAYywBMKrPbZBCmNHqxIfrj9h2muljJPSzoMjZAsyd4GkveMtCXsa91yzcxc043WErGcRRR2kOznxB5zESqNFNSP0VTvpsZBbh4aXEaN1vPgDHhd40xeUVMisu", true);
-//    xhr.open("GET", "https://graph.facebook.com/me?access_token=CAACEdEose0cBAGNYmIKzi5rd2GFn5oNRREa8wIDXgStaPADXJZCiVoEw5hBSoAPPkLWQzNLhKZAY9qygpZCL44T2N90gGDmpR2Gq4z8sTzkOneQzxq4LXK7MxnezPbPj5RyJYxUxQGl3XZBVi3tGGYyFg6ShXMf5FWKTm4441fxxUzwfNfQpfoqksj8LYJrdbkTiZBy71ajtsZAmRFwhCw", true);
 
-    xhr.send();
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        // xhr1 = new XMLHttpRequest();
+        xhr2 = new XMLHttpRequest();
+    } else { // code for IE6, IE5
+        // xhr1 = new ActiveXObject("Microsoft.XMLHTTP");
+        xhr2 = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    // xhr1.onreadystatechange = callfacebook;
+    xhr2.onreadystatechange = callgoogleplus;
+
+    // var fbtoken = "CAACEdEose0cBAFlsISihjUscFuXZA1wQlXqKiuAl0eAnlOQXCJ9inb0Ig5hMoGBNOUOTCtaiq6wbeMPmEgaGw6qQCMqu9LwM5zOGdIZBtqO8bDQgdPkylCZCNZBWoFqHiG9IIJaQ38EpxB5yCu5chUfB75CFUPOGlXqZClC5A4sqkMmIa1bBNqOIwPJ7lPj9RKxnEGQaXp2z30LtcBRNy";
+    var gptoken = "ya29.WQGldPsiiWX6VQxAYupINHN36Uqn5zQ1YhurUEl3ER-Zs1xVHXW6wq7TjkhyvlylOxqnRZX5Y6uQ0w";
+
+    // xhr1.open("GET", "https://graph.facebook.com/search?q=" + query + "&type=place&fields=id,name,picture&access_token=" + fbtoken, true);
+    xhr2.open("GET", "https://www.googleapis.com/plus/v1/people?query=" + query + "&access_token=" + gptoken, true);
+
+    // xhr1.send();
+    xhr2.send();
 }
-function callback(){
-    //console.log(xhr.readyState);
-    if (xhr.readyState === 4) {
-//            alert(xhr.status);
-        // status of 200 signifies sucessful HTTP call
-        if (xhr.status === 200) {
-            //clearoption();
-//            alert(xhr.responseText);
-            var JSONresp = JSON.parse(xhr.responseText);
-//            id =  JSONresp["name"]; 
 
-            data =  JSONresp["data"];
-            var name = [data.length]; 
-            var id = [data.length];
-            var x = [data.data]
+function callfacebook() {
+        //console.log(xhr1.readyState);
+        if (xhr1.readyState === 4) {
+            //            alert(xhr1.status);
+            // status of 200 signifies sucessful HTTP call
+            if (xhr1.status === 200) {
+                //clearoption();
+                //            alert(xhr1.responseText);
+                var JSONresp = JSON.parse(xhr1.responseText);
+                //            id =  JSONresp["name"]; 
 
-            var myNode = document.getElementById("result");
+                data = JSONresp["data"];
+                var name = [data.length];
+                var id = [data.length];
+                var x = [data.data]
 
-            while (myNode.firstChild) {
-                myNode.removeChild(myNode.firstChild);
-            }
-            for (var i = 0, max = data.length ; i < max; i++) {
-                name = data[i].name;
-                id = data[i].id;          
+                var myNode = document.getElementById("result");
 
-                var d = document.createElement("div");
-                d.setAttribute("class","result inline");
+                while (myNode.firstChild) {
+                    myNode.removeChild(myNode.firstChild);
+                }
+                var max = 50;
+                for (var i = 0; i < max; i++) {
+                    name = data[i].name;
+                    id = data[i].id;
 
-                var a = document.createElement("a");
-                a.setAttribute("href", x.url);
+                    var div = document.createElement("div");
+                    div.setAttribute("class", "result inline");
 
-                var img = document.createElement("img");
-                img.setAttribute("src", "https://graph.facebook.com/"+id+"/picture");
+                    //show image
+                    var a = document.createElement("a");
+                    a.setAttribute("href", x.url);
+                    var img = document.createElement("img");
+                    img.setAttribute("class", "inline");
+                    img.setAttribute("src", "https://graph.facebook.com/" + id + "/picture");
+                    a.appendChild(img);
 
-                var src = document.createElement("img");
-                src.setAttribute("src", "image/bg.png");
-                src.setAttribute("width", "20px");
+                    //For showing source
+                    var divs = document.createElement("div");
+                    divs.setAttribute("id", "source");
+                    divs.setAttribute("class", "inline");
 
-                var p = document.createElement("p");
-                var t = document.createTextNode(name); 
-                a.appendChild(img);
-                p.appendChild(t);
-                d.appendChild(p);
-                d.appendChild(a);
-                d.appendChild(src);
-                document.getElementById("result").appendChild(d);
-            }
-            
+                    var srcboth = document.createElement("img");
+                    srcboth.setAttribute("src", "image/bg.png");
 
-            //type=JSONresp["name"].toString();
-//            code=  JSONresp["code"].toString();
-            //document.getElementById("profile").innerHTML = name;
+                    var srcfb = document.createElement("img");
+                    srcfb.setAttribute("src", "image/fb.png");
+
+                    var srcgp = document.createElement("img");
+                    srcgp.setAttribute("src", "image/gp.png");
+
+                    divs.appendChild(srcfb);
+                    div.appendChild(divs);
+
+
+                    //Show name
+                    var p = document.createElement("p");
+                    var text = document.createTextNode(name);
+                    p.appendChild(text);
+
+                    div.appendChild(p);
+                    div.appendChild(a);
+                    div.appendChild(divs);
+                    document.getElementById("result").appendChild(div);
+                }
+
+
+                //type=JSONresp["name"].toString();
+                //            code=  JSONresp["code"].toString();
+                //document.getElementById("profile").innerHTML = name;
             }
         }
-    }
+    } // end of function
+
+function callgoogleplus() {
+        //console.log(xhr1.readyState);
+        if (xhr2.readyState === 4) {
+            //            alert(xhr2.status);
+            // status of 200 signifies sucessful HTTP call
+            if (xhr2.status === 200) {
+
+                var JSONresp = JSON.parse(xhr2.responseText);
+
+                items = JSONresp["items"];
+
+                var name = [items.length];
+                var id = [items.length];
+                
+
+                var myNode = document.getElementById("result");
+
+                while (myNode.firstChild) {
+                    myNode.removeChild(myNode.firstChild);
+                }
+                var max = 50;
+                for ( i = 0; i < max; i++) {
+                    console.log(items[i].displayName);
+                    name = items[i].displayName;
+                    id = items[i].id;
+
+                    var div = document.createElement("div");
+                    div.setAttribute("class", "result inline");
+
+                    //show image
+                    var a = document.createElement("a");
+                    var img = document.createElement("img");
+                    img.setAttribute("class", "inline");
+                    img.setAttribute("src", "https://graph.facebook.com/" + id + "/picture");
+                    a.appendChild(img);
+
+                    //For showing source
+                    var divs = document.createElement("div");
+                    divs.setAttribute("id", "source");
+                    divs.setAttribute("class", "inline");
+
+                    var srcboth = document.createElement("img");
+                    srcboth.setAttribute("src", "image/bg.png");
+
+                    var srcfb = document.createElement("img");
+                    srcfb.setAttribute("src", "image/fb.png");
+
+                    var srcgp = document.createElement("img");
+                    srcgp.setAttribute("src", "image/gp.png");
+
+                    divs.appendChild(srcgp);
+                    div.appendChild(divs);
+
+
+                    //Show name
+                    var p = document.createElement("p");
+                    var text = document.createTextNode(name);
+                    p.appendChild(text);
+
+                    div.appendChild(p);
+                    div.appendChild(a);
+                    div.appendChild(divs);
+                    document.getElementById("result").appendChild(div);
+                }
+
+
+                //type=JSONresp["name"].toString();
+                //            code=  JSONresp["code"].toString();
+                //document.getElementById("profile").innerHTML = name;
+            }
+        }
+    } // end of function
